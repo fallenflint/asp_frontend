c929a74c4b0d2aaff31f9550a92d76eaca1ffde1
Igor Flint <netwamp@gmail.com>
2024-10-26 01:40:39 +0700

    Fix projects fields and urls

--- frontend/src/views/Projects.js
+++ frontend/src/views/Projects.js
@@ -8,11 +8,10 @@ import { config } from '../config';
 const columns = [
   { field: 'inner_num', headerName: 'Внутренний Номер', flex: 1 },
   { field: 'name', headerName: 'Наименование', flex: 1 },
-  { field: 'customer', headerName: 'Заказчик', flex: 1 },
-  { field: 'inner_company', headerName: 'Внутренняя Компания', flex: 1 },
-  { field: 'manager', headerName: 'Менеджер', flex: 1 },
+  { field: 'customer', headerName: 'Заказчик', flex: 1, autocomplete: config.COMPANIES },
+  { field: 'inner_company', headerName: 'Внутренняя Компания', flex: 1, autocomplete: config.COMPANIES },
+  { field: 'manager', headerName: 'Менеджер', flex: 1, autocomplete: config.USERS},
   { field: 'status', headerName: 'Статус', flex: 1 },
-  { field: 'sum', headerName: 'Сумма', flex: 1 },
   { field: 'updated_at', headerName: 'Дата Обновления', flex: 1, valueFormatter: (value)=>formatDate(value)},
 ];
 
@@ -20,7 +19,7 @@ const columns = [
 const ProjectList = () => {
 
   return (
-    <ObjectList columns={columns} api_url={'//localhost/api/v2/projects/'}/>
+    <ObjectList columns={columns} api_url={config.PROJECTS}/>
     );
 };
 
@@ -29,21 +28,21 @@ export default ProjectList;
 
 const ProjectDetail = () => {
   return (
-    <ObjectDetail api_url={'//localhost/api/v2/projects/'} columns={columns}/>
+    <ObjectDetail api_url={config.PROJECTS} columns={columns}/>
   );
 };
 
 
 const ProjectCreate = () => {
   return (
-    <ObjectForm api_url={'//localhost/api/v2/projects/'} columns={columns} caption="Добавить новый проект" />
+    <ObjectForm api_url={config.PROJECTS} columns={columns} caption="Добавить новый проект" />
   );
 }
 
 
 const ProjectEdit = () => {
   return (
-    <ObjectForm api_url={'//localhost/api/v2/projects/'} columns={columns} caption="Редактировать проект" />
+    <ObjectForm api_url={config.PROJECTS} columns={columns} caption="Редактировать проект" />
   );
 }
 
