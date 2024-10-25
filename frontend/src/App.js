aac14af3f3d85960bcbe62e8b0e7e73ea6b732fa
Igor Flint <netwamp@gmail.com>
2024-10-25 21:19:17 +0700

    Add new routes to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,38 +2,36 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import { useState } from 'react';
 import LoginPage from './Login';
 import Logout from './Logout';
-import ProjectList, {ProjectDetail} from './Projects';
-import CompanyList, {CompanyDetail, CompanyCreate} from './Companies';
-import UserList, {UserDetail} from './Users';
+import ProjectList, {ProjectDetail, ProjectCreate, ProjectEdit} from './Projects';
+import CompanyList, {CompanyDetail, CompanyCreate, CompanyEdit} from './Companies';
+import UserList, {UserDetail, UserCreate, UserEdit} from './Users';
 import Layout from './Layout';
 import { isAuthenticated, setIsAuthenticated } from './utils/api';
 
 
-const PublicRoutes = ({isAuthenticated}) => (
-  <Routes>
-    <Route path="/login" element={<LoginPage setIsAuthenticated={setIsAuthenticated} />} />
-    <Route path="/logout" element={<Logout setIsAuthenticated={setIsAuthenticated} />} />
-  </Routes>
-);
-
-const ProtectedRoutes = () => (
-  <Routes>
-    <Route path="/companies" element={ <Layout><CompanyList/></Layout>} />
-    <Route path="/companies/detail/:uuid" element={ <Layout><CompanyDetail/></Layout>} />
-    <Route path="/companies/create" element={ <Layout><CompanyCreate/></Layout>} />
-    <Route path="/users" element={ <Layout><UserList/></Layout>} />
-    <Route path="/users/detail/:uuid" element={ <Layout><UserDetail/></Layout>} />
-    <Route path="/projects" element={ <Layout><ProjectList/></Layout>} />
-    <Route path="/projects/detail/:uuid" element={ <Layout><ProjectDetail/></Layout>} />
-    <Route path="/" element={ <Navigate to='/projects'/>} />
-  </Routes>
-);
-
 const App = () => {
   return (
     <Router>
-      <PublicRoutes isAuthenticated={isAuthenticated}/>
-      <ProtectedRoutes />
+      <Routes>
+        <Route path="/login" element={<LoginPage setIsAuthenticated={setIsAuthenticated} />} />
+        <Route path="/logout" element={<Logout setIsAuthenticated={setIsAuthenticated} />} />
+        <Route path="/companies" element={ <Layout><CompanyList/></Layout>} />
+        <Route path="/companies/create" element={ <Layout><CompanyCreate/></Layout>} />
+        <Route path="/companies/:uuid" element={ <Layout><CompanyDetail/></Layout>} />
+        <Route path="/companies/:uuid/edit" element={ <Layout><CompanyEdit/></Layout>} />
+        {/*<Route path="/companies/:uuid/delete" element={ <Layout><CompanyDelete/></Layout>} />*/}
+        <Route path="/users" element={ <Layout><UserList/></Layout>} />
+        <Route path="/users/:uuid" element={ <Layout><UserDetail/></Layout>} />
+        <Route path="/users/create" element={ <Layout><UserCreate/></Layout>} />
+        <Route path="/users/:uuid/edit" element ={ <Layout><UserEdit/></Layout>} />
+        {/*<Route path="/users/:uuid/delete" element={ <Layout><UserDelete/></Layout>} />*/}
+        <Route path="/projects" element={ <Layout><ProjectList/></Layout>} />
+        <Route path="/projects/create" element={ <Layout><ProjectCreate/></Layout>} />
+        <Route path="/projects/:uuid" element={ <Layout><ProjectDetail/></Layout>} />
+        <Route path="/projects/:uuid/edit" element={ <Layout><ProjectEdit/></Layout>} />
+        {/*<Route path="/projects/:uuid/delete" element={ <Layout><ProjectDelete/></Layout>} />*/}
+        <Route path="/" element={ <Navigate to='/projects'/>} />
+      </Routes>
     </Router>
   );
 };
